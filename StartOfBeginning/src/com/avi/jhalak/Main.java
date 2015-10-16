7e28aea60c0c2869e81b7af418f390ef9650b1a6
Avinash Shaw <avinashuskc.com>
2015-10-16 15:54:44 -0400

    initial check

---
+++ StartOfBeginning/src/com/avi/jhalak/Main.java
@@ -0,0 +1,18 @@
+package com.avi.jhalak;
+
+public class Main {
+	
+	public static void main(String[] args){
+		startWalking newBeginning = new startWalking();
+		newBeginning.begin();
+	}
+	
+
+			
+
+	private static void displayGreeting(){
+		System.out.print("Hello ");
+		System.out.println("How are you?");
+	}
+	
+}
---
+++ StartOfBeginning/src/com/avi/jhalak/startWalking.java
@@ -0,0 +1,9 @@
+package com.avi.jhalak;
+
+public class startWalking {
+
+	public void begin() {
+	System.out.println("New");
+	}
+
+}
